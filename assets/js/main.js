51a2242a978b35cc96e76bf6a1a96f86e5975ab3
sudammondal <sudammondal02@gmail.com>
2021-07-18 23:19:26 +0600

    tab item addhtml

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,4 @@ function closeNav() {
 }
 
 
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "400px";
-//   }
-  
-//   function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//   }
-
 
