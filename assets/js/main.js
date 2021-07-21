db75aa600af3245555463928f462a0824e2e65b7
sudammondal <sudammondal02@gmail.com>
2021-07-22 01:43:31 +0600

    game woro work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+//=============sitebar js=============//
 function openNav() {
   document.getElementById("mySidenav").style.width = "290px";
 }
@@ -7,5 +7,85 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+//==============header js===========//
+var prevScrollpos = window.pageYOffset; 
+window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("Header").style.top = "0";
+      } else {
+        document.getElementById("Header").style.top = "-108px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
 
+    //============= tab items js===============//
+    function showhide() {
+      var x = document.querySelectorAll("#London");
+      var i;
+      if ($("#Paris") === "block" || $("#London")==="block") {
+        $("#Paris").hide();
+        $("#London").hide();
+        $("#Tokyo").show();
+        } else {
+          $("#Paris").show();
+        $("#London").show();
+        $("#Tokyo").hide();
+      }
+    }
+  
+  function button_1(){
+  
+    $('#London').show();
+    $('#London1').show();
+    $('#London2').show();
+  
+    $('#Paris').hide();
+    $('#Paris1').hide();
+    $('#Paris2').hide();
+  
+    $('#Tokyo').hide();
+    $('#Tokyo1').hide();
+    $('#Tokyo2').hide();
+  }
+  
+  function button_2(){
+    $('#Paris').show();
+    $('#Paris1').show();
+    $('#Paris2').show();
+  
+    $('#Tokyo').hide();
+    $('#Tokyo1').hide();
+    $('#Tokyo2').hide();
+  
+    $('#London').hide();
+    $('#London1').hide();
+    $('#London2').hide();
+  }
+  function button_3(){
+    $('#Tokyo').show();
+    $('#Tokyo1').show();
+    $('#Tokyo2').show();
+  
+    $('#Paris').hide();
+    $('#Paris1').hide();
+    $('#Paris2').hide();
+  
+    
+    $('#London').hide();
+    $('#London1').hide();
+    $('#London2').hide();
+  }
 
+  //================slick slider js===============//
+  $(document).ready(function(){
+    $('.world_slider_items').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      // autoplay: true,
+      variableWidth: true
+    });
+  });
